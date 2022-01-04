e17160108992c7a7662ea4b354679d9125af50ef
truongjr <nguyentruongjr@gmail.com>
2022-01-04 21:07:49 +0700

    update UI

---
+++ src/main/java/library_management/NoScalingIcon.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library_management;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author nguye
+ */
+public class NoScalingIcon implements Icon{
+    private Icon icon;
+
+    NoScalingIcon(Icon icon) {
+        this.icon = icon;
+    }
+
+//    NoScalingIcon(ImageIcon imageIcon) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+
+    public int getIconWidth() {
+        return icon.getIconWidth();
+    }
+
+    public int getIconHeight() {
+        return icon.getIconHeight();
+    }
+
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        Graphics2D g2d = (Graphics2D) g.create();
+
+        AffineTransform at = g2d.getTransform();
+
+        int scaleX = (int) (x * at.getScaleX());
+        int scaleY = (int) (y * at.getScaleY());
+
+        int offsetX = (int) (icon.getIconWidth() * (at.getScaleX() - 1) / 2);
+        int offsetY = (int) (icon.getIconHeight() * (at.getScaleY() - 1) / 2);
+
+        int locationX = scaleX + offsetX;
+        int locationY = scaleY + offsetY;
+
+        //  Reset scaling to 1.0 by concatenating an inverse scale transfom
+
+        AffineTransform scaled = AffineTransform.getScaleInstance(1.0 / at.getScaleX(), 1.0 / at.getScaleY());
+        at.concatenate(scaled);
+        g2d.setTransform(at);
+
+        icon.paintIcon(c, g2d, locationX, locationY);
+
+        g2d.dispose();
+    }
+}
---
+++ src/main/java/library_management/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library_management;
+
+/**
+ *
+ * @author nguye
+ */
+public class main {
+    public static void main (String [] args){
+//        tên dang nh?p 
+    }
+}
